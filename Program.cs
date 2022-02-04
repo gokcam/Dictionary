fd447df959207a3e1620dd7a838d285e4dad04a0
Gökhan Çam <76878009+gokcam@users.noreply.github.com>
2022-02-04 18:24:33 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int,string> kullanıcılar = new Dictionary<int, string>();
+            kullanıcılar.Add(55,"Gökhan Çam");
+            kullanıcılar.Add(25,"Sümeyye Aydin");
+
+
+            Console.WriteLine(kullanıcılar[55]);
+
+            foreach(var kullanıcı in kullanıcılar)
+            {
+                Console.WriteLine(kullanıcı);
+            }
+
+            // Count
+                Console.WriteLine(kullanıcılar.Count());
+
+            // Contains
+                Console.WriteLine(kullanıcılar.ContainsKey(25));
+                Console.WriteLine(kullanıcılar.ContainsValue("aLİ"));
+
+            // Remove
+            kullanıcılar.Remove(55);
+            foreach(var kullanıcı in kullanıcılar)
+            {
+                Console.WriteLine(kullanıcı);
+            }
+
+            // keys and value
+
+            foreach(var kullanıcı in kullanıcılar.Keys)
+            {
+                Console.WriteLine(kullanıcı);
+            }
+
+            foreach(var kullanıcı in kullanıcılar.Values)
+            {
+                Console.WriteLine(kullanıcı);
+            }
+
+
+
+        }
+    }
+}
